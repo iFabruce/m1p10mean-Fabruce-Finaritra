c223a469900f0adc620555e16c405cfd2366c94d
iFabruce <fabrucemaminiaina@gmail.com>
2024-02-22 13:56:49 +0300

    update user nodemailer

--- node-mongodb-server/app/config/email.config.js
+++ node-mongodb-server/app/config/email.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    user: 'isendmailbot@gmail.com',
+    user: 'mami2fabruce@gmail.com',
     clientId: "991878170827-kj64rdok2i3oj8oe1r0ie04aja2rq42s.apps.googleusercontent.com",
     clientSecret: "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",
     refreshToken: "1//04OexT0s0xOb9CgYIARAAGAQSNwF-L9IrEXZk1wpb55i4SItDQVn_JyMM9oPkoazgJRXrcQdAkIJ3N0PO2wjrGSWiCvz3QOiU35s"
