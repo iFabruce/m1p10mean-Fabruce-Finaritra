216d6f45086823a8e56205e966d824637aed2f80
Finaritra Andriantahiana <fyandriantahiana@gmail.com>
2024-02-27 23:37:32 +0300

    modification back depense

--- node-mongodb-server/app/controllers/spent.controller.js
+++ node-mongodb-server/app/controllers/spent.controller.js
@@ -21,7 +21,7 @@ exports.findAll = (req, res) => {
   };
 
   exports.findOne = (req, res) => {
-    const {month,year,type}=request.params;
+    const {month,year,type}=req.params;
     spentService.findOne(month,year,type)
     .then(data => {
       console.log("data: ",data)
@@ -42,3 +42,29 @@ exports.findAll = (req, res) => {
         res.status(500).send({ message: err.message || "Erreur" });
       });
   };
+
+  exports.update = (req, res) => {
+    spentService.updateSpent(req.body)
+      .then(
+        data => {
+          res.send(data);
+        }
+      )
+      .catch(error =>{
+        res.status(500).send({message: error.message || "Erreur"})
+      })
+  };
+  
+  exports.calculateTotalPrice = (req, res) => {
+    const { month, year } = req.params;
+    spentService.calculateTotalPrice(month, year)
+        .then(data => {
+            res.status(200).send({ totalPrice: data }); // Utilisez un code d'état HTTP valide (par exemple, 200 OK)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send({ message: err.message || "Erreur" });
+        });
+};
+
+
