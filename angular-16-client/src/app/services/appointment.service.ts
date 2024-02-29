2778d757f5b8625bbe28d4a8549bba298e9908a8
iFabruce <fabrucemaminiaina@gmail.com>
2024-02-29 13:44:04 +0300

    localhost to prod

--- angular-16-client/src/app/services/appointment.service.ts
+++ angular-16-client/src/app/services/appointment.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const baseUrl = "http://localhost:8080/api/appointment/";
+const baseUrl = "https://m1p10mean-fabruce-finaritra.onrender.com/api/appointment/";
 
 @Injectable({
   providedIn: 'root'
@@ -24,6 +24,6 @@ export class AppointmentService {
     return this.http.post(`${baseUrl}create`, {date, hour, employeeId, clientId, serviceId, email})
   }
   sendEmail(email: any, subject: any,  date: any, heure: any, duration: any, service: any,employee: any){
-    return this.http.post('http://localhost:8080/api/email/reservation', {email, subject, date, heure, duration, service, employee})
+    return this.http.post('https://m1p10mean-fabruce-finaritra.onrender.com/api/email/reservation', {email, subject, date, heure, duration, service, employee})
   }
 }
